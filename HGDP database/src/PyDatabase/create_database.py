8be25e4b94a767c7f55ffb1b02ef5a2c3cd60b94
gioby <dalloliogm@gmail.com>
2008-10-23 16:52:35 +0200

    added commetns on declarative mapping

--- HGDP database/src/PyDatabase/create_database.py
+++ HGDP database/src/PyDatabase/create_database.py
@@ -9,22 +9,34 @@ Refer to sqlalchemy manual, and in particular to this passage:
 
 """
 
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import create_engine, Table, Column, Integer, String, Float, MetaData, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 
 # this will create a temporary database in Ram memory, using an engine called sqlite. 
 engine = create_engine('sqlite:///:memory:', echo=True)
-metadata = MetaData()
 
+# I will use declaration mapping in this code. This means that both the tables and the instances of every row will be defined at the same time.
+# see 'object mapping' on sqlalchemy manual.
+# see also http://www.sqlalchemy.org/docs/05/ormtutorial.html#datamapping_declarative
 Base = declarative_base()
 
 class SNP(Base):
+    """
+    Table 'SNP'.
+    
+    This class represents both the table SNP, and the structure  
+    """
     __tablename__ = 'SNP'
     
-    SNP_Id = Column(String(10), primary_key=True),
-    chromosome = Column(String(10)),        # should be a choice between 1-22-XY
-    physical_position = Column(Integer),
-    genetic_position = Column(Integer),
-    reference_allele_freq = Column()
+    SNP_Id                  = Column(String(10), primary_key=True)
+    chromosome              = Column(String(10))        # should be a choice between 1-22-XY
+    physical_position       = Column(Integer)
+    genetic_position        = Column(Integer)
+    reference_allele_freq   = Column(Float)
+    
+    def __init__(self):
+        # this method will be launched when you create 
+    
+    
         
