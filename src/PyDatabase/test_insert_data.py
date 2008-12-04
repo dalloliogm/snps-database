29bd0f9c7b57568e04587657d02258bb60e9c3db
dalloliogm <dalloliogm@gmail.com>
2008-12-04 19:12:21 +0100

    writing an unittest

--- src/PyDatabase/test_insert_data.py
+++ src/PyDatabase/test_insert_data.py
@@ -1,40 +1,51 @@
 #!/usr/bin/env python
+"""
+Unittest for HGDP database
+"""
+import unittest
+
 from connection import session 
 from elixir import metadata, setup_all, create_all
 from schema import Individual, Population, SNP, RefSeqGene
 
-def setup():
-    """create a test database in RAM memory"""
-    metadata.bind = 'sqlite:///:memory:'
-    setup_all()
-    create_all()
-
-def test_insert():
-    """test the insertion of a few individuals and populations"""
-    me = Individual('Giovanni')
-    hannibal = Individual('Annibal')
+class TestHGDPDatabase(unittest.TestCase):
+    """
+    Test the HGDP database APIs (sqlalchemy + elixir)
+    """
     
-    # create populations
-    italians = Population('Italians')
-    cartaginians = Population('Cartaginians')    # would be better to use more hgdp-related examples
+    def setUp(self):
+        """create a test database in RAM memory"""
+        metadata.bind = 'sqlite:///:memory:'
+#        metadata.bind.echo = True
+        setup_all()
+        create_all()
     
-    me.population = italians
-    hannibal.population = cartaginians   
+    def test_insert(self):
+        """test the insertion of a few individuals and populations"""
+        me = Individual('Giovanni')
+        hannibal = Individual('Annibal')
+        
+        # create populations
+        italians = Population('Italians')
+        cartaginians = Population('Cartaginians')    # would be better to use more hgdp-related examples
+        
+        me.population = italians
+        hannibal.population = cartaginians   
+        
+        session.commit()
+        session.clear()
+        
+    def test_duplicated_individual(self):
+        self.assertRaises(Individual('Giovanni'), 'IntegrityError')
     
-    session.commit()
-    session.clear()
-
-
-def test_alotof_insert():
-    individuals = [Individual('Ind' + str(i)) for i in range(1000)]
-    session.commit()
-    session.query(Individual).limit(100)
-
-def test_query():
-    print Individual.query().all()
+    def test_alotof_insert(self):
+        individuals = [Individual('Ind' + str(i+1)) for i in range(1000)]
+        session.commit()
+        session.query(Individual).limit(100)
+    
+    def test_query(self):
+        print Individual.query().all()
     
 if __name__ == '__main__':
-    setup()
-    test_insert()
-    test_alotof_insert()
-    print session.query(Individual).all()
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestHGDPDatabase)
+    unittest.TextTestRunner(verbosity=2).run(suite)
