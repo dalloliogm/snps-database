2731ddd5d421eff2d05dbe2a83d843b019dbee26
dalloliogm <dalloliogm@gmail.com>
2009-01-22 16:35:41 +0100

    a test unit for the rosenberg parser

---
+++ src/HGDPIO/test_hgdpio.py
@@ -0,0 +1,46 @@
+import unittest
+import commands
+import logging
+
+from schema.debug_database import *
+from elixir import drop_all
+
+class test_rosenberg(unittest.TestCase):
+    """
+    Test the rosenberg parser
+    """
+
+    testfile = 'rosenberg_sample.txt'
+    _db_is_set = 0
+
+    def setupdb(self):
+        """
+        SetUpAll method
+        """
+        logging.basicConfig(level=logging.DEBUG)
+        self._db_is_set = 1
+        from parsers import rosenberg_parser
+        rosenberg_parser(open(self.testfile, 'r'))
+
+    def setUp(self):
+        print self._db_is_set
+        create_all()
+        if self._db_is_set == 0:
+            self.setupdb()
+#
+    def tearDown(self):
+        drop_all()
+
+    def test_individuals(self):
+        """test how many individuals"""
+        Individual.query().all()
+        logging.debug('dsadas')
+        print 'dsad'
+
+    def test_SameNumberOfFields(self):
+        """
+        test if every line in the parser is splitted in the same number of fields
+        """
+        pass
+
+
