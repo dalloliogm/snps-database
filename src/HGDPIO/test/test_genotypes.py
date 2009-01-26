be794fd44d2bbe572ddb2500ba0d8a10bd44030a
dalloliogm <dalloliogm@gmail.com>
2009-01-26 11:58:55 +0100

    added a test for genotype_index

--- src/HGDPIO/test/test_genotypes.py
+++ src/HGDPIO/test/test_genotypes.py
@@ -14,17 +14,17 @@ class test_genotypes(unittest.TestCase):
     individualsfile = 'test/rosenberg_sample.txt'
     testfile = 'test/genotypes_sample.txt'
 #    _db_is_set = False
-    known_individuals = {'HGDP00218': (),
-                    'HGDP00248': (),
-                    'HGDP00232': (),
-                    'HGDP00222': (),
-                    'HGDP00228': (),
-                    'HGDP00239': (),
-                    'HGDP00258': (),
-                    'HGDP00247': (),
-                    'HGDP00234': (),
-                    'HGDP00214': (),
-                    'HGDP00262': (),}
+    known_individuals = {'HGDP00218': {'index': 0,},
+                    'HGDP00248': {'index': 1,},
+                    'HGDP00232': {'index': 2,},
+                    'HGDP00222': {'index': 3,},
+                    'HGDP00228': {'index': 4,},
+                    'HGDP00239': {'index': 5,},
+                    'HGDP00258': {'index': 6,},
+                    'HGDP00247': {'index': 7,},
+                    'HGDP00234': {'index': 8,},
+                    'HGDP00214': {'index': 9,},
+                    'HGDP00262': {'index': 10,},}
     known_snps = ('rs4911642', 'rs2027653', 'rs5747620', 'rs9605903', 'rs5747968', 'rs2236639', 
             'rs5747999', 'rs11089263', 'rs2096537', 'rs9604959', 'rs9604967', 'rs4819849', 
             'rs9605028','rs1892844', 'rs361973', 'rs2845371', 'rs16981507')
@@ -54,7 +54,10 @@ class test_genotypes(unittest.TestCase):
 
     def test_genotypeindex(self):
         """test if the right index is added to every individual"""
-        pass
+        for ind_id in self.known_individuals.keys():
+            ind = Individual.query.filter_by(name=ind_id).one()
+            print ind.genotype_index
+            self.assertEqual(ind.genotype_index, self.known_individuals[ind_id]['index'])
 
 
     def test_SameNumberOfFields(self):
