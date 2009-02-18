53db80b30ef2dd865f4ab92746f210414441197c
dalloliogm <dalloliogm@gmail.com>
2009-02-18 15:53:21 +0100

    fixed sorting of snps in Gene.get_snps, + doctests

--- src/schema/schema.py
+++ src/schema/schema.py
@@ -601,16 +601,16 @@ class RefSeqGene(Entity):
 
         >>> snp1 = SNP('snp1', chromosome = 11, physical_position = 500)    # not included
         >>> snp2 = SNP('snp2', chromosome = 11, physical_position = 700)    # included
-        >>> snp4 = SNP('snp4', chromosome = 11, physical_position = 800)    # included
-        >>> snp3 = SNP('snp3', chromosome = 11, physical_position = 1000)   # included
+        >>> snp3 = SNP('snp3', chromosome = 11, physical_position = 800)    # included
+        >>> snp4 = SNP('snp4', chromosome = 11, physical_position = 1000)   # included
         >>> snp5 = SNP('snp5', chromosome = 11, physical_position = 1200)   # included
         >>> snp6 = SNP('snp6', chromosome = 11, physical_position = 1500)   # included
         >>> snp7 = SNP('snp7', chromosome = 11, physical_position = 1600)   # not included
         >>> snp8 = SNP('snp8', chromosome = 1, physical_position = 1000)   # not included (other chromosome)
-
+        >>> snp4b = SNP('snp4b', chromosome = 11, physical_position = 1100) # included and after snp4
 
         >>> gene1.get_snps(300, 300)
-        [SNP snp2, SNP snp3, SNP snp4, SNP snp5, SNP snp6]
+        [SNP snp2, SNP snp3, SNP snp4, SNP snp4b, SNP snp5, SNP snp6]
 
         >>> session.close()
         """
@@ -627,7 +627,7 @@ class RefSeqGene(Entity):
         snps = SNP.query().filter_by(chromosome = self.chromosome).\
                                 filter(SNP.physical_position >= lower_limit).\
                                 filter(SNP.physical_position <= upper_limit).all()  
-        snps = sorted(snps, cmp=lambda x, y: x.physical_position > y.physical_position)
+        snps.sort(cmp=lambda x, y: x.physical_position - y.physical_position)
         return snps
     
     
