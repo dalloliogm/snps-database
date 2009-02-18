1f40772efb8c1586c7917058392a6f7fff553d20
dalloliogm <dalloliogm@gmail.com>
2009-02-18 11:10:24 +0100

    fixed some problems with data dir

--- data/Annotations/ucsc/loaddata.sql
+++ data/Annotations/ucsc/loaddata.sql
@@ -1,7 +1,1 @@
-/* #LOAD DATA LOCAL INFILE 'genes_refseq' into table refseqgenes FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' (name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds, alternateName, cdsStartStat, cdsEndStat) */
-
-LOAD DATA LOCAL INFILE 'genes_refseq_mar2006', into table refseqgenes FIELDS TERMINATED BY '\t' ENCLOSED BY "" (ncbi_transcript_id, chromosome, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds, alternateName, cdsStartStat, cdsEndStat, exonFrames)
-
-/* genomic_build */
-
-/* name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds, alternateName, cdsStartStat, cdsEndStat, exonFrames */
+LOAD DATA LOCAL INFILE 'genes_refseq' into table refseqgenes FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' (name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds, alternateName, cdsStartStat, cdsEndStat, exonFrames);
