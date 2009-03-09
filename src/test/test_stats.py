308ecf295cba232f1b8150a8249957901fc88f42
dalloliogm <dalloliogm@gmail.com>
2009-03-09 13:07:17 +0100

    added a test for iHS Stats upload

---
+++ src/test/test_stats.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""
+Tests the setup of the stats table, and some queries on it
+"""
+
+
+import unittest as U
+import logging
+from schema.connection import *
+from sqlalchemy.sql import func
+
+
+class KnowValues(U.TestCase):
+    """
+    Tests for known values
+    """
+
+    @classmethod
+    def setUpAll(cls):
+        logging.basicConfig(level = logging.DEBUG)
+
+    def test_AllStatsHaveBeenUploadedPerChromosome(self):
+        chromosomes = [str(chr) for chr in range(1, 22)]
+        
+        for chr in chromosomes:
+            stats = Stats.query().filter(Stats.population_key == 'eur').filter(Stats.snp.has(chromosome = str(chr))).limit(1).all()
+            logging.debug(stats)
+
+            print
+            print chr
+            print stats
+
+            self.assertNotEqual(stats, [])
+
+
