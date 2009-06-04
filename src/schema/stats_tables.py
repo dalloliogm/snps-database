c8fab6667110fc8a3f7b1c63afd0aebedf69e133
dalloliogm <dalloliogm@gmail.com>
2009-06-04 14:22:02 +0200

    stub for CLR stats

--- src/schema/stats_tables.py
+++ src/schema/stats_tables.py
@@ -17,28 +17,39 @@ import operator
 class _BaseSNPStats(Entity):
     snp_id = Field(Text(20))
     population_key = Field(Text(50))
+    value = Field(Float(2, 32), index=True)
 
-    def __init__(self, snp_id, popkey):
+    def __init__(self, snp_id, popkey, value):
         self.snp_id = snp_id.lower().strip()
         self.population_key = popkey.upper().strip()
+        self.value = value
 
     def __repr__(self):
         return 'stat on snp %s on pop %s' % (self.snp_id, self.population_key)
 
+class _BaseStat(Entity):
+    position = Text(30)
+    chromosome = Text(6)
+
 class iHS(_BaseSNPStats):
-    using_options(tablename = 'ihs', inheritance='concrete')
-    pass
+    using_options(tablename = 'ihs', inheritance = 'concrete')
+
+class XPEHH(_BaseSNPStats):
+    using_options(tablename = 'xpehh', inheritance = 'concrete')
 
 class Fst(_BaseSNPStats):
-    using_options(tablename = 'fst', inheritance='concrete')
+    using_options(tablename = 'fst', inheritance = 'concrete')
 
 def _test():
+    import random
     from elixir import session, metadata, setup_all, create_all
     connection_line = 'sqlite:///:memory:'
     metadata.bind = connection_line
 
     setup_all()
     create_all()
+
+    [iHS('snp_%s' % i, 'pop', random.random()) for i in xrange(10)]
 
 if __name__ == '__main__':
     _test()
