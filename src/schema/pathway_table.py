75dd8b74cb55c0148d664d02ea73695e6fc6b31a
dalloliogm <dalloliogm@gmail.com>
2009-06-15 11:35:16 +0200

    added some fields to pathway_table. How to create the inverse relationship?

--- src/schema/pathway_table.py
+++ src/schema/pathway_table.py
@@ -4,8 +4,36 @@ Table to store pathways
 """
 
 from schema import *
+from elixir import Entity, Field, Unicode, Integer, UnicodeText, String, Text, Float
+from elixir import ManyToOne, OneToMany, OneToOne, ManyToMany, DateTime
+from elixir import metadata, using_options
+from config import connection_line
+import logging
+import operator
 
 
+class Pathway(Entity):
+    """
+    >>> from debug_database import *
+    >>> from elixir import setup_all, create_all
+    >>> metadata.bind = 'sqlite:///:memory:'
+    >>> setup_all(); create_all()
+    >>> print metadata
+    MetaData(Engine(sqlite:///:memory:))
+
+    >>> path = Pathway()
+    >>> snps = [SNP(str(x)) for x in xrange(10)]
+    >>> genes = [RefSeqTranscript(str(x)) for x in xrange(4)]
+
+    >>> path.genes = genes
+    >>> print path.genes
+    """
+    title = Field(Text)
+    kegg_id = Field(Text(20))
+
+    genes = OneToMany('RefSeqTranscript')
+    snps = OneToMany('SNP')
+    
 
 
 
