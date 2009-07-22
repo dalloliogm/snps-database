42c0de02327c95f5107c3de050ad8165e5c6db94
dalloliogm <dalloliogm@gmail.com>
2009-07-22 14:21:44 +0200

    test for XPEHH

--- src/test/test_stats.py
+++ src/test/test_stats.py
@@ -32,4 +32,27 @@ class TestStats(U.TestCase):
 
             self.assertNotEqual(stats, [])
 
+class TestXPEHH(U.TestCase):
+    """
+    Tests for known values
+    """
+
+    @classmethod
+    def setUpAll(cls):
+        logging.basicConfig(level = logging.DEBUG)
+
+    def test_ForEveryChromosomeThereIsAtLeastOneStatSaved(self):
+        chromosomes = [str(chr) for chr in range(1, 22)]
+        chromosomes.extend(['X', 'Y'])
+        
+        for chr in chromosomes:
+            stats = XPEHH.query().filter(XPEHH.snp.has(chromosome = str(chr))).limit(1).all()
+            logging.debug(stats)
+
+            print
+            print chr
+            print stats
+
+            self.assertNotEqual(stats, [])
+
 
